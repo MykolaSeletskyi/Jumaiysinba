92c2b52605852ebcfc93626e49a410b9bdc8794e
Mykola_Seletskyi <seletskyi@sunpoint.com.ua>
2022-07-19 22:58:12 +0300

    develop to main

---
+++ Jumaiysinba/Startup.cs
@@ -0,0 +1,110 @@
+using Infrastructure.Data;
+using Jumaiysinba.Inits;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Jumaiysinba
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Include;
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            });
+
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin()
+                                                             .AllowAnyHeader()
+                                                             .AllowAnyMethod());
+            });
+            
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+            services
+                .UseDatabaseContext(Configuration)
+                .UseUsefulNuGets(Configuration)
+                .UseAuthJWT(Configuration)
+                .UseServices()
+                .AddEndpointsApiExplorer()
+                .AddSwaggerGen()
+                .UseSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app
+                .UseStaticFiles()
+                .UseSpaStaticFiles();
+
+            app.UseRouting();
+
+            app.UseCors("AllowOrigin");
+
+            app.UseHttpsRedirection();
+
+            app
+                .UseSwagger()
+                .UseSwaggerUI();
+
+            app.UseAppStaticFiles();
+
+            app
+                .UseAuthentication()
+                .UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+
+            app.SeedData();
+        }
+    }
+}
