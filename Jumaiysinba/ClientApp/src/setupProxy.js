b37112b11bc0e6f4535d75a5ed0dcd6919f4abc4
Mykola Seletskyi <seletskyi.mykola@gmail.com>
2022-09-08 16:54:33 +0300

    asd2da

--- Jumaiysinba/ClientApp/src/setupProxy.js
+++ Jumaiysinba/ClientApp/src/setupProxy.js
@@ -1,5 +1,3 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
 module.exports = function (app) {
   app.use((_, res, next) => {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
